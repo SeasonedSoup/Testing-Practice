b0b6dc80bfe1bccad3bd2e358656e2daee9d0c63
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-05-22 21:36:27 +0800

    Finish CaesarCipher and Tests
    Utilized modulo rememebeered ascii constraints
    of lower and uppercase letters and just let symbols
    and more just reappend again to the new string

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,8 +1,23 @@
 export const caesarCipher = (s, shift) => {
     let caesarString = ""
+
     for (let i = 0; i < s.length; i++) {
         const shiftedChar = s[i].charCodeAt(0)
-        caesarString += String.fromCharCode(shiftedChar + shift)
+        if (shiftedChar >= 65 && shiftedChar <= 90) {
+            upperCaseBound();
+        } else if (shiftedChar >= 97 && shiftedChar <= 122) {
+            lowerCaseBound();
+        } else {
+            caesarString += String.fromCharCode(shiftedChar)
+        }
+
+        function upperCaseBound() {
+            caesarString += String.fromCharCode((shiftedChar - 65 + shift) % 26 + 65)
+        }
+        function lowerCaseBound() {
+            caesarString += String.fromCharCode((shiftedChar - 97 + shift) % 26 + 97)
+        } 
     }
     return caesarString
 }
+
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,5 +1,13 @@
 import {caesarCipher} from './caesarCipher'
 
-test('Turn Hello shift by 1 into Ifmmp', () => {
+test('Turn Hello shifted by 1 into Ifmmp', () => {
     expect(caesarCipher('Hello', 1)).toBe('Ifmmp')
+})
+
+test('Turn I ate a HoTDog shifted by 5 into N fyj f MtYItl', () => {
+    expect(caesarCipher('I ate a HoTDog', 5)).toBe('N fyj f MtYItl')
+})
+
+test('Turn &8394921 why shifted by 26 into &8394921 why', () => {
+    expect(caesarCipher('&8394921 why', 26)).toBe('&8394921 why')
 })
